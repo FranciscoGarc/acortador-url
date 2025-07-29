745703cdb04386c6720a452a05dd74183a37e992
FranciscoGarc <20220326@ricaldone.edu.sv>
2025-07-28 20:16:34 -0600

    Refactor URL storage to use in-memory ConcurrentHashMap instead of Redis

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,12 +5,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    val port = System.getenv("PORT")?.toInt() ?: 8080
-
-    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
-
 fun Application.module() {
     configureSerialization()
     configureRouting()
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -10,11 +10,8 @@ import io.ktor.server.routing.*
 import kotlinx.html.*
 import kotlinx.serialization.Serializable
 import java.util.concurrent.ConcurrentHashMap
-import redis.clients.jedis.Jedis
 
-//val urlStorage = ConcurrentHashMap<String, String>()
-val redisUrl = System.getenv("REDIS_URL") ?: "redis://localhost:6379"
-val jedis = Jedis(redisUrl) // Creamos una instancia del cliente de Redis
+val urlStorage = ConcurrentHashMap<String, String>()
 
 fun Application.configureRouting() {
     routing {
@@ -22,21 +19,21 @@ fun Application.configureRouting() {
             val request = call.receive<ShortenRequest>()
             val longUrl = request.url
 
-            val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234TUE"
+            // Genera un código corto aleatorio
+            val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
             val shortCode = (1..6).map { alphabet.random() }.joinToString("")
 
-            jedis.set(shortCode, longUrl)
+            urlStorage[shortCode] = longUrl
 
+            // Construye la URL corta completa para la respuesta
             val baseUrl = System.getenv("BASE_URL") ?: "http://${call.request.host()}:${call.request.port()}"
             val shortUrl = "$baseUrl/$shortCode"
 
             call.respond(ShortenResponse(shortUrl = shortUrl))
         }
         get("/{shortCode}") {
-            val shortCode = call.parameters["shortCode"] ?: ""
-
-            // --- Leer de Redis ---
-            val longUrl = jedis.get(shortCode)
+            val shortCode = call.parameters["shortCode"]
+            val longUrl = urlStorage[shortCode]
 
             if (longUrl != null) {
                 call.respondRedirect(longUrl, permanent = true)
